8b4ca96b752709540e4dc38882a86ea19958002c
JoanJuan10 <iijoanjuan10@gmail.com>
2021-02-05 13:56:26 +0100

    Ejercicio 4

---
+++ EX4/EX4/Models/Reserva.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EX4.Models
+{
+    public partial class Reserva
+    {
+        public string DNI { get; set; }
+        public string NumSerie { get; set; }
+        public DateTime Comienzo { get; set; }
+        public DateTime Fin { get; set; }
+
+        public virtual Investigador Investigadores { get; set; }
+        public virtual Equipo Equipos { get; set; }
+    }
+}
