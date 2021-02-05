7663639336133b4b7cdb59297046305e8718ffe1
JoanJuan10 <iijoanjuan10@gmail.com>
2021-02-05 12:27:16 +0100

    Ejercicio 2

---
+++ EX2/EX2/Models/Cientifico.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EX2.Models
+{
+    public partial class Cientifico
+    {
+        public Cientifico()
+        {
+            CientificoAsignadoA = new HashSet<Asignado>();
+        }
+
+        public string DNI { get; set; }
+        public string NomApels { get; set; }
+
+        public virtual ICollection<Asignado> CientificoAsignadoA { get; set; }
+
+    }
+}
---
+++ EX2/EX2/Models/Proyecto.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace EX2.Models
+{
+    public partial class Proyecto
+    {
+        public Proyecto()
+        {
+            ProyectoAsignadoA = new HashSet<Asignado>();
+        }
+        public string Id { get; set; }
+        public string Nombre { get; set; }
+        public int Horas { get; set; }
+
+        public virtual ICollection<Asignado> ProyectoAsignadoA { get; set; }
+
+
+    }
+}
